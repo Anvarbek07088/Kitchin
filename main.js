547fa3fc2699ee96beb0d973b2cf79d4253cba4f
Anvarbek <anvarbektoshmamatov888@gmail.com>
2024-02-19 18:32:11 +0500

    yakun

---
+++ main.js
@@ -0,0 +1,40 @@
+let orderList = [];
+let totalPrice = 0;
+
+function order(item) {
+    orderList.push(item);
+    updateOrderList();
+}
+
+function updateOrderList() {
+    const listElement = document.getElementById('order-list');
+    listElement.innerHTML = '';
+    orderList.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        listElement.appendChild(li);
+    });
+}
+
+function calculateTotal() {
+    totalPrice = 0;
+    orderList.forEach(item => {
+        switch(item) {
+            case 'Palov':
+                totalPrice += 25000;
+                break;
+            case 'Lagmon':
+                totalPrice += 20000;
+                break;
+            case 'Kola':
+                totalPrice += 5000;
+                break;
+            case 'Ayran':
+                totalPrice += 3000;
+                break;
+            // Boshqa taomlar va ichimliklar uchun case larni qo'shing
+        }
+    });
+    const totalElement = document.getElementById('total');
+    totalElement.textContent = 'Jami: ' + totalPrice + ' so\'m';
+}
